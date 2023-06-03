3554ed2c6bc005efea391b529fde0c9012b31715
sotoaao-graw <sot3180@gmail.com>
2023-06-03 22:00:27 +0900

    first commit

---
+++ src/main/kotlin/com/book/manager/application/service/BookService.kt
@@ -0,0 +1,20 @@
+package com.book.manager.application.service
+
+import com.book.manager.domain.model.BookWithRental
+import com.book.manager.domain.repository.BookRepository
+import org.springframework.stereotype.Service
+import java.lang.IllegalArgumentException
+
+@Service
+class BookService(
+    private val bookRepository: BookRepository
+) {
+
+    fun getList(): List<BookWithRental> {
+        return bookRepository.findAllWithRental()
+    }
+
+    fun getDetail(bookId: Long): BookWithRental {
+        return bookRepository.findWithRental(bookId) ?: throw IllegalArgumentException("存在しない書籍ID : $bookId")
+    }
+}
---
+++ src/main/kotlin/com/book/manager/domain/enum/RoleType.kt
@@ -0,0 +1,5 @@
+package com.book.manager.domain.enum
+
+enum class RoleType {
+    ADMIN, USER
+}
